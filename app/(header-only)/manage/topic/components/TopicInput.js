84cd1e23c33b8b791264f5a54e28a2746937998a
Minh Chau <buitongminhchau01032002@gmail.com>
2023-06-27 21:21:23 +0700

    add manage topic

---
+++ app/(header-only)/manage/topic/components/TopicInput.js
@@ -0,0 +1,108 @@
+'use client';
+import { useState } from 'react';
+
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { API } from '~/constants';
+import { userSelector } from '~/redux/selectors';
+
+function TopicInput({ topic, onChange }) {
+    const [editing, setEditing] = useState(false);
+    const [name, setName] = useState(topic.name);
+    const showUpdateNoti = () => toast.success('Chỉnh sửa thành công!');
+    const showDeleteNoti = () => toast.success('Xoá sửa thành công!');
+    const showErorrNoti = () => toast.error('Có lỗi xảy ra!');
+    const user = useSelector(userSelector);
+
+    function updateTopic() {
+        fetch(`${API}/${topic._id}`, {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${user?.token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: name }),
+        })
+            .then((res) => res.json())
+            .then((resJson) => {
+                if (resJson.error_key) {
+                    showErorrNoti();
+                    return;
+                }
+                setEditing(false);
+                showUpdateNoti();
+                onChange();
+            })
+            .catch((err) => {
+                console.log(err);
+                showErorrNoti();
+            });
+    }
+
+    function deleteTopic() {
+        fetch(`http://localhost:8080/api/categories/${topic._id}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${user?.token}`,
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                    showErorrNoti();
+                    return;
+                }
+                onChange();
+                showDeleteNoti();
+            })
+            .catch((err) => {
+                console.log(err);
+                showErorrNoti();
+            });
+    }
+
+    return !editing ? (
+        <div className="flex border-b py-2">
+            <div className="flex-1">{topic.name}</div>
+            <button
+                onClick={() => setEditing(true)}
+                className="flex h-7 items-center justify-center rounded-md border border-primary px-5 text-sm font-medium text-primary-dark transition hover:bg-primary/10"
+            >
+                Edit
+            </button>
+            {/* <button
+                onClick={() => deleteTopic()}
+                className="ml-2 flex h-7 items-center justify-center rounded-md bg-red-500 px-3 text-sm font-medium text-white transition hover:bg-red-600"
+            >
+                Xoá
+            </button> */}
+        </div>
+    ) : (
+        <div className="flex items-center">
+            <div className="mr-2 flex-1">
+                <input
+                    type="text"
+                    className="text-input"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                />
+            </div>
+            <button
+                onClick={() => updateTopic()}
+                className="flex h-7 items-center justify-center rounded-md border border-primary px-5 text-sm font-medium text-primary-dark transition hover:bg-primary/10"
+            >
+                Update
+            </button>
+            <button
+                onClick={() => setEditing(false)}
+                className="ml-2 flex h-7 items-center justify-center rounded-md bg-red-500 px-3 text-sm font-medium text-white transition hover:bg-red-600"
+            >
+                Close
+            </button>
+        </div>
+    );
+}
+
+export default TopicInput;
---
+++ app/(header-only)/manage/topic/page.js
@@ -0,0 +1,100 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { API } from '~/constants';
+import { userSelector } from '~/redux/selectors';
+import TopicInput from './components/TopicInput';
+
+export default function ManageTopicPage() {
+    const user = useSelector(userSelector);
+    const [topicName, setTopicName] = useState('');
+    // const [topicContent, setTopicContent] = useState('');
+
+    const [topics, setTopics] = useState([]);
+    useEffect(() => {
+        getTopics();
+    }, []);
+
+    function getTopics() {
+        fetch(`${API}/topics`, {
+            headers: {
+                Authorization: `Bearer ${user?.token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((resJson) => {
+                if (resJson.error_key) {
+                    console.log(resJson);
+                    setTopics([]);
+                    return;
+                }
+                setTopics(resJson.data);
+            })
+            .catch((err) => {
+                console.log(err);
+                setTopics([]);
+            });
+    }
+
+    function createTopic() {
+        fetch(`${API}/topics`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${user?.token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: topicName }),
+        })
+            .then((res) => res.json())
+            .then((resJson) => {
+                if (resJson.error_key) {
+                    console.log(resJson);
+                    toast.error(resJson.message || 'Something went wrong');
+                    return;
+                }
+                setTopicName('');
+                toast.success('Created topic!');
+                getTopics();
+            })
+            .catch((err) => {
+                console.log(err);
+                toast.error('Something went wrong');
+            });
+    }
+
+    return (
+        <div>
+            <h2 className="mb-3 text-xl font-bold">Manage Topic</h2>
+
+            <div className="mb-3 mt-2 flex items-center space-x-2">
+                <input
+                    type="text"
+                    className="text-input flex-1"
+                    onChange={(e) => setTopicName(e.target.value)}
+                    value={topicName}
+                    placeholder="Topic name"
+                />
+                {/* <input
+                    type="text"
+                    className="text-input flex-1"
+                    onChange={(e) => setTopicContent(e.target.value)}
+                    value={topicContent}
+                    placeholder="Content"
+                /> */}
+                <button
+                    onClick={() => createTopic()}
+                    className="flex h-9 items-center justify-center rounded-md bg-primary px-5 text-sm font-medium text-white transition hover:bg-primary-dark"
+                >
+                    Create topic
+                </button>
+            </div>
+            <div className="mt-3 space-y-3 bg-white p-3">
+                {topics?.map((topic) => (
+                    <TopicInput topic={topic} key={topic._id} onChange={getTopics} />
+                ))}
+            </div>
+        </div>
+    );
+}
