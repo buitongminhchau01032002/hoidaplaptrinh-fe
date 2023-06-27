6fb2c55e29c07d607ff71960951ac54be4d621c6
Minh Chau <buitongminhchau01032002@gmail.com>
2023-06-27 18:50:05 +0700

    🔥 get topic

--- app/(header-sidebar)/components/Sidebar/Sidebar.js
+++ app/(header-sidebar)/components/Sidebar/Sidebar.js
@@ -1,73 +1,40 @@
 'use client';
 
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { API } from '~/constants';
 import colorizeCategory from '~/utils/colorizeCategory';
 
 export default function Sidebar() {
+    const [topics, setTopics] = useState();
+    useEffect(() => {
+        fetch(`${API}/topics`)
+            .then((res) => res.json())
+            .then((resJson) => {
+                if (resJson.error_key) {
+                    return;
+                }
+                setTopics(resJson.data);
+            });
+    }, []);
     return (
         <div>
             <div className="mb-5">
                 <p className="text-xl font-semibold">Topics</p>
                 <div className="mt-3 flex flex-wrap">
-                    <button
-                        style={{
-                            backgroundColor: colorizeCategory({
-                                createdAt: '2023-04-07T05:49:10.259+00:00',
-                            }),
-                        }}
-                        className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
-                    >
-                        Javascript
-                    </button>
-                    <button
-                        style={{
-                            backgroundColor: colorizeCategory({
-                                createdAt: '2023-04-07T05:42:23.259+00:00',
-                            }),
-                        }}
-                        className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
-                    >
-                        HTML, CSS
-                    </button>
-                    <button
-                        style={{
-                            backgroundColor: colorizeCategory({
-                                createdAt: '2023-04-07T05:20:53.259+00:00',
-                            }),
-                        }}
-                        className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
-                    >
-                        Golang
-                    </button>
-                    <button
-                        style={{
-                            backgroundColor: colorizeCategory({
-                                createdAt: '2023-04-07T05:10:28.259+00:00',
-                            }),
-                        }}
-                        className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
-                    >
-                        Pascal
-                    </button>
-                    <button
-                        style={{
-                            backgroundColor: colorizeCategory({
-                                createdAt: '2023-04-03T05:30:03.259+00:00',
-                            }),
-                        }}
-                        className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
-                    >
-                        Java
-                    </button>
-                    <button
-                        style={{
-                            backgroundColor: colorizeCategory({
-                                createdAt: '2023-04-03T05:30:40.259+00:00',
-                            }),
-                        }}
-                        className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
-                    >
-                        C#
-                    </button>
+                    {topics?.map((topic) => (
+                        <Link
+                            key={topic._id}
+                            href={'/?topic=413'}
+                            style={{
+                                backgroundColor: colorizeCategory(topic),
+                            }}
+                            className=" mb-1 mr-1 rounded px-3 py-1 font-semibold text-white"
+                        >
+                            {topic.name}
+                        </Link>
+                    ))}
                 </div>
             </div>
             <div className="mb-5">
